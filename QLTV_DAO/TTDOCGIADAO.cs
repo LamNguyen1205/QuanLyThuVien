c4bf4484a939decbf81e1c3c65b5cae3b911db37
LamNguyen1205 <duylam.nguyenhuu0504@gmail.com>
2021-09-30 23:09:51 +0700

    Add project files.

---
+++ QLTV_DAO/TTDOCGIADAO.cs
@@ -0,0 +1,52 @@
+﻿using QLTV_DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLTV_DAO
+{
+    public class TTDOCGIADAO
+    {
+        private static TTDOCGIADAO instance;
+
+        public static TTDOCGIADAO Instance
+        {
+            get { if (instance == null) instance = new TTDOCGIADAO(); return instance; }
+            set => instance = value;
+        }
+        private TTDOCGIADAO() { }
+        //QuanLyThuVienEntities db = new QuanLyThuVienEntities();
+        public List<TTDOCGIADTO> GetListDocGiaInfo()
+        {
+            List<TTDOCGIADTO> listDocGiaInfo = new List<TTDOCGIADTO>();
+            using (QuanLyThuVienEntities db = new QuanLyThuVienEntities())
+            {
+                var data = ((from u in db.DOCGIAs
+                           join n in db.ACCOUNTs on u.IDAccount equals n.IDAccount
+                           select new { u.MaDocGia, u.HoTen, u.NgaySinh, u.DiaChi, u.Email, u.NgayLapThe, u.NgayHetHan, u.MaLoaiDocGia, u.IDAccount, n.PasswordAccount, u.SDT})).ToList();
+                foreach (var item in data)
+                {
+                    TTDOCGIADTO ttdg = new TTDOCGIADTO
+                    {
+                        MaDocGia = item.MaDocGia,
+                        HoTen = item.HoTen,
+                        NgaySinh = item.NgaySinh,
+                        DiaChi = item.DiaChi,
+                        Email = item.Email,
+                        NgayLapThe = item.NgayLapThe,
+                        NgayHetHan = item.NgayHetHan,
+                        MaLoaiDocGia = item.MaLoaiDocGia,
+                        IDAccount = item.IDAccount,
+                        PasswordAccount = item.PasswordAccount,
+                        SDT = item.SDT,
+                    };
+                    listDocGiaInfo.Add(ttdg);
+                }
+            }
+            return listDocGiaInfo;
+        }
+    }
+}
